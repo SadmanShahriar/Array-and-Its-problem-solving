4c79a744819cc33cadd66ebdea5956b057f32622
Sadman Shahriar <118925483+SadmanShahriar@users.noreply.github.com>
2022-12-26 14:39:04 +0600

    Add files via upload

---
+++ javaArray/src/javaArray/javaArrray.java
@@ -0,0 +1,47 @@
+package javaArray;
+
+public class javaArrray {
+
+	public static void main(String[] args) {
+		
+//		//methord
+//		findEvenOdd(29);
+//		
+//		System.out.println(sum(6,9));
+//
+//	}
+//	
+//	public static void findEvenOdd(int num)
+//	{
+//		if (num%2==0)
+//			System.out.println(num+" is even");
+//		else
+//			System.out.println(num+" is odd");
+//		
+//	}
+//	
+//	public static float sum(float numberOne, float numberTwo) {
+//		return numberOne + numberTwo;
+	
+		
+		//array
+		
+		int a [] = new int[5];
+		a[0]=11;
+		a[1]=20;
+		a[2]=13;
+		a[3]=26;
+		a[4]=17;
+		
+		for(int i=0; i<a.length; i++) {
+		System.out.println(a[i]);
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+}
